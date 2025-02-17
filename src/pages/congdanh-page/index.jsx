c0262277e8b0a35e66dc6e4664a562551eef020d
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2025-02-17 18:38:06 +0700

    setup router

---
+++ src/pages/congdanh-page/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CongDanh = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default CongDanh
---
+++ src/pages/phuoctan-page/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhuocTan = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default PhuocTan
