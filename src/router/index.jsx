ea9e92832f6576f161344547981eedb84cb3a491
HaoPhan205 <130329315+HaoPhan205@users.noreply.github.com>
2025-02-19 11:31:43 +0700

    .'

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from "react-router-dom"
-import PhuocTan from "../pages/phuoctan-page"
-import KangMin from "../pages/kangmin-page"
-import CongDanh from "../pages/congdanh-page"
-import PhanHao from "../pages/phanhao-page"
-import Home from "../pages/home"
+import { Route, Routes } from "react-router-dom";
+import PhuocTan from "../pages/phuoctan-page";
+import KangMin from "../pages/kangmin-page";
+import CongDanh from "../pages/congdanh-page";
+import PhanHao from "../pages/phanhao-page";
+import Home from "../pages/home";
 const AppRouter = () => {
-    return (
-        <Routes>
-             <Route path={"/"} element={<Home />} />
-              <Route path={"phuoctan"} element={<PhuocTan />} />
-              <Route path={"kangmin"} element={<KangMin />} />
-              <Route path={"congdanh"} element={<CongDanh />} />
-              <Route path={"phanhao"} element={<PhanHao />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path={"/"} element={<Home />} />
+      <Route path={"phuoctan"} element={<PhuocTan />} />
+      <Route path={"kangmin"} element={<KangMin />} />
+      <Route path={"congdanh"} element={<CongDanh />} />
+      <Route path={"phanhao"} element={<PhanHao />} />
+    </Routes>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
